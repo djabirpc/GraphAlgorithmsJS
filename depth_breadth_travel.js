43972c0ce2b0ccd6b428036a9f95b61b404c897c
djabirpc <djabir.pc@outlook.fr>
2021-08-08 23:36:44 +0100

    yes

---
+++ depth_breadth_travel.js
@@ -0,0 +1,49 @@
+//**************** Depth ****************//
+
+const depthFirstPrint = (graph, source) => {
+    const stack = [source];
+
+    while (stack.length > 0) {
+        const current = stack.pop();
+        console.log(current);
+        for (let neighbor of graph[current]) {
+            stack.push(neighbor);
+        }
+    }
+};
+
+const depthFirstPrintRecersive = (graph, source) => {
+    console.log(source);
+    for (let neighbor of graph[source]) {
+        depthFirstPrintRecersive(graph, neighbor);
+    }
+};
+
+//**************** Breadth ****************//
+const breathFirstPrint = (graph, source) => {
+    const queue = [source];
+    while (queue.length > 0) {
+        const current = queue.shift();
+        console.log(current);
+        for(let neighbor of graph[current]){
+            queue.push(neighbor);
+        }
+    }
+};
+
+
+
+const graph = {
+    a: ['c', 'b'],
+    b: ['d'],
+    c: ['e'],
+    d: ['f'],
+    e: [],
+    f: []
+};
+
+//depthFirstPrint(graph,'a');
+//depthFirstPrintRecersive(graph,'a');
+
+breathFirstPrint(graph,'a');
+
